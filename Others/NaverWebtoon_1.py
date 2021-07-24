7c3a666f216e793f8b78c5c77878a50f9698efa6
sinw212 <sinw212@gmail.com>
2021-07-25 01:04:32 +0900

    210706

--- Others/NaverWebtoon_1.py
+++ Others/NaverWebtoon_1.py
@@ -9,7 +9,26 @@ def solution(S, pattern):
         for j in range((len(S) - len(pattern)) + 1):
             if i == "".join(S[j:len(pattern) + j]):
                 count += 1
+
+    # count = 0
+    # for i in range(len(S) - len(pattern) + 1):
+    #     data = []
+    #     for j in range(i, i + len(pattern)):
+    #         data.append(S[j])
+    #     if sorted(data) == sorted(list(pattern)):
+    #         count += 1
     return count
+
+#permutations 안쓰고 푸는 방법
+# def solution(S, pattern):
+#     count = 0
+#     for i in range(len(S)-len(pattern)+1):
+#         data = []
+#         for j in range(i,i+len(pattern)):
+#             data.append(S[j])
+#         if sorted(data) == sorted(list(pattern)):
+#             count += 1
+#     return count
 
 print(solution("tegsornamwaresomran","ransom")) #2
 print(solution("wreawerewa","ware")) #4
